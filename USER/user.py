c7d14fd430a2f7c4ff7a27d5bfcc544a08108be6
DEX <denis-tochilov@yandex.ru>
2024-09-27 19:46:00 +0300

    Создан Проект для User

---
+++ USER/user.py
@@ -0,0 +1,17 @@
+# Создание класс для User
+class User:
+    # Конструктор класса User
+    def __init__(self):
+        self.name = ""  # создание и инициализация переменной name
+        self.surname = "" # создание и инициализация переменной surname
+        self.patronymic = "" # создание и инициализация переменной patronymic
+        self.age = 0    # # создание и инициализация переменной age
+
+user = User() # создание переменной объекта для User
+user.name = "Denis" # присваиваем переменной имена
+user.surname = "Tochilov" # присваиваем переменной фамилия
+user.patronymic = "Nikolaevich"  # присваиваем переменной отечество
+user.age = 34 # присваиваем переменной возраста
+
+# выводим на экран
+print(user.name, user.surname, user.patronymic, user.age)
