f181a94d0f8c4c4213fa89fa3c1184c44b4d2350
TochilovDenis <denis-tochilov@yandex.ru>
2024-09-27 20:22:34 +0300

    Изменение коструктор параметризованыый

--- USER/user.py
+++ USER/user.py
@@ -1,18 +1,15 @@
 # Создание класс для User
 class User:
-    # Конструктор класса User
-    def __init__(self):
+    # Конструктор параметризованный класса User
+    def __init__(self, name, surname, patronymic, age):
         self.name = ""  # создание и инициализация переменной name
         self.surname = "" # создание и инициализация переменной surname
         self.patronymic = "" # создание и инициализация переменной patronymic
         self.age = 0    # # создание и инициализация переменной age
 
-user = User() # создание переменной объекта для User
-user.name = "Denis" # присваиваем переменной имена
-user.surname = "Tochilov" # присваиваем переменной фамилия
-user.patronymic = "Nikolaevich"  # присваиваем переменной отечество
-user.age = 34 # присваиваем переменной возраста
+user = User("Denis", "Tochilov", "Nikolaevich", 34)
+
 user.new_attribute = "Это не существующий при создании класса аттрибут теперь там существует"
 
 # выводим на экран
-print(user.name, user.surname, user.patronymic, user.age, user.new_attribute)
+print(user, user.new_attribute)
