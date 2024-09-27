9e3f318ebc2b33378303b522472d757e1ba81098
DEX <denis-tochilov@yandex.ru>
2024-09-27 19:51:06 +0300

    Добавлен new_attribute

--- USER/user.py
+++ USER/user.py
@@ -12,6 +12,7 @@ user.name = "Denis" # присваиваем переменной имена
 user.surname = "Tochilov" # присваиваем переменной фамилия
 user.patronymic = "Nikolaevich"  # присваиваем переменной отечество
 user.age = 34 # присваиваем переменной возраста
+user.new_attribute = "Это не существующий при создании класса аттрибут теперь там существует"
 
 # выводим на экран
-print(user.name, user.surname, user.patronymic, user.age)
+print(user.name, user.surname, user.patronymic, user.age, user.new_attribute)
