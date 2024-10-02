d360d0bf0d2f99b080e2544bf244120f37d1c5d4
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-02 20:16:06 +0300

    Создан функцию генератор объекта House с названием get_random_house, чтобы она возвращала объект House со случайно сгенерированными параметрами.

--- APARTMENT/apartment.py
+++ APARTMENT/apartment.py
@@ -15,9 +15,16 @@ class Apartment:
     def __len__(self):
         return len(self.numberOfOccupants)
 
+def get_random_apartment():
+    return Apartment(randint(1, 5), randint(1,4), randint(1,9), randint(1, 36))
+
+apartment = [get_random_apartment() for _ in range(10)]
+for ap in apartment:
+    print(ap)
+
 
 class House:
-    def __init__(self, list_of_apartments, house_number, number_of_floors, number_of_entrances):
+    def __init__(self, house_number, number_of_floors, number_of_entrances):
         self.list_of_apartments = [] # список квартир
         self.house_number = house_number # номер дома
         self.number_of_floors = number_of_floors # количество этажей
@@ -41,9 +48,10 @@ class House:
             print(a)
 
 
-def get_random_apartment():
-    return Apartment(randint(1, 5), randint(1,4), randint(1,9), randint(1, 36))
+def get_random_house(amount_of_random_apartments: int = 0) -> House:
+    house: House = House(str(randint(1, 100)), randint(1, 9), randint(1, 10))
 
-apartment = [get_random_apartment() for _ in range(10)]
-for ap in apartment:
-    print(ap)
+    for _ in range(amount_of_random_apartments):
+        house.add_apartment(get_random_apartment())
+
+    return house
